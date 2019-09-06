a879417162204af515401202a916adc5701fc420
Terrance Medina <terrance.medina@gmail.com>
2019-09-06 09:05:55 -0400

    Fix Bug in room price calculation where half hours were being dropped in mixed day and night reservations.

--- checkout/room_info.php
+++ checkout/room_info.php
@@ -45,7 +45,6 @@ $row = mysql_fetch_array($result);
 $cancel_id = $row['id'];
 
 // create the room reservation transaction if not exist and if there is no cancellation.
-//
 if ( $rm_id != "" && $cancel_id == "" )
 {
     $query = "SELECT id FROM reservation_transaction
@@ -71,117 +70,17 @@ if ( $rm_id == "" )
 }
 else
 {
-    // get the room transaction details
-    $query = "SELECT rt.id AS rt_id, re.room_id,
-                     rt.id as room_id, room_name, room_charge_day, room_charge_night, room_charge_drummer, room_charge_employee_day, room_charge_employee_night,
-                     re.start_time as start_time, re.end_time as end_time
-                FROM reservation_entry re, reservation_transaction rt, reservation_room rm
-               WHERE re.id = rt.reservation_entry_id
-                 AND rm.id = rt.reservation_room_id
-                 AND re.id =  $reservation_id";
+    $charge = calc_room_charge($reservation_id);
 
-    //echo $query;
-    // die and show mysql error number and messages, if there is any error with query
-    $result = mysql_query($query) or die ("ERROR: " . mysql_errno() . "-" . mysql_error() );
-
-    if (mysql_num_rows($result)>0)
+    if ( $room_trans_already_exist != "YES")
     {
-        while($row = mysql_fetch_array($result))
-        {
-            $rt_id       = $row['rt_id'];
-            $room_id    = $row['room_id'];
-            $room_name = $row['room_name'];
-            $room_charge_day = $row['room_charge_day'];
-            $room_charge_night = $row['room_charge_night'];
-            $start_time  = $row['start_time'];
-            $end_time    = $row['end_time'];
-            $duration_hour  = calc_utf_period_diff( $end_time, $start_time )/60;
-            $str_start_time = strtotime( utf_period_2_date($start_time) );
-            $str_end_time   = strtotime( utf_period_2_date($end_time) );
-            $day_number     = date('N', $str_start_time);
-            $start_hour     = date('H', $str_start_time);
-            $end_hour       = date('H', $str_end_time);
-            $room_charge_drummer = $row['room_charge_drummer'];
-            $room_charge_employee_day = $row['room_charge_employee_day'];
-            $room_charge_employee_night = $row['room_charge_employee_night'];
-
-            //echo "start time: " . utf_period_2_date($start_time) . " end_time: " . utf_period_2_date($end_time) . "<br/>";
-
-            //echo "day number: $day_number start hour: $start_hour <br/>";
-
-            //TEST
-            //echo "debug: day_number is manually set to 4<br/>";
-            //$day_number = 4;
-
-            // weekdays or weekend
-            //
-            if ( $day_number > 5 ) //weekend
-            {
-                //echo "weekend charge";
-                $charge = $duration_hour * $room_charge_night;
-                $duration_night = $duration_hour;
-            }
-            else // weekday
-            {
-                // start time after 5pm, use night charge
-                if ( $start_hour >= 17 || ($start_hour >= 0 && $start_hour <= 2)  )
-                {
-                    //echo "weekday night charge";
-                    $charge = $duration_hour * $room_charge_night;
-                    $duration_night = $duration_hour;
-                }
-                else // start time before 5pm, use day charge
-                {
-                    if ( $end_hour <= 17 || ($end_hour >= 0 && $end_hour <= 2) ) //end time before 5 pm
-                    {
-                        //echo "weekday day charge";
-                        $charge = $duration_hour * $room_charge_day;
-                        $duration_day = $duration_hour;
-                    }
-                    else // mixed
-                    {
-
-                        $duration_day = 17 - $start_hour;
-
-                        if ($end_hour == 0)
-                        {
-                            $end_hr = 24;
-                        }
-                        elseif ($end_hour == 1)
-                        {
-                            $end_hr = 25;
-                        }
-                        elseif ($end_hour == 2)
-                        {
-                            $end_hr = 26;
-                        }
-                        $duration_night = $end_hour - 17;
-
-
-                        $charge = ($duration_day * $room_charge_day) + ($duration_night * $room_charge_night);
-
-                        //echo "weekday day $room_charge_day and night charge $room_charge_night - dur day: $duration_day dur night: $duration_night <br/>";
-
-                    }
-                }
-            }
-
-            //DEBUG print to the box in invoice.php
-            //echo "<br/><br/> <b> $room_name: $duration_hour hr  = $ $charge </b>";
-
-            if ( $room_trans_already_exist != "YES")
-            {
-                // update the room reservation transaction
-                $query = "UPDATE reservation_transaction SET amount = $charge
-                           WHERE reservation_entry_id = $reservation_id
-                             AND reservation_room_id = $rm_id";
-                //echo $query;
-                mysql_query($query) or die ("ERROR: " . mysql_errno() . "-" . mysql_error() );
-            }
-        }
+        // update the room reservation transaction
+        $query = "UPDATE reservation_transaction SET amount = $charge
+                  WHERE reservation_entry_id = $reservation_id
+                  AND reservation_room_id = $rm_id";
+        //echo $query;
+        mysql_query($query) or die ("ERROR: " . mysql_errno() . "-" . mysql_error() );
     }
-
-
 
     if ( $room_mc_id != "")
     {
@@ -206,34 +105,19 @@ else
             $mc_name = $row['mc_name'];
         }
 
+        $discounted_charge = $charge;
         // if it's a drummer rate
         if ( $mc_name == "Drummer Rate" )
         {
-            //echo "in drummer rate $duration_hour $room_charge_drummer";
-            $disc_amt = $charge - ($duration_hour * $room_charge_drummer);
+            $discounted_charge = calc_room_charge($reservation_id, 1);
+            $disc_amt = ($charge - $discounted_charge) * -1;
         }
         elseif ( $mc_name == "Employee Rate")
         {
-            //echo "in employee rate";
-            if ( $duration_day > 0 && $duration_night == 0 ) // day only
-            {
-                //create discount tran in the amount of $amount
-                $disc_amt = $charge;
-            }
-            elseif ( $duration_day == 0 && $duration_night > 0 ) // night only
-            {
-                $disc_amt = $duration_night * $room_charge_employee_night;
-            }
-            elseif ( $duration_day > 0 && $duration_night > 0 )
-            {
-                $disc_amt = ($duration_day * $room_charge_day) + ( ($duration_night * $room_charge_night) -  ($duration_night * $room_charge_employee_night) );
-            }
+            $discounted_charge = calc_room_charge($reservation_id, 2);
+            $disc_amt = ($charge - $discounted_charge) * -1;
+            //echo "discounted charge:".$discounted_charge."<br/>";
         }
-
-        $disc_amt *= -1;
-        //echo "<br/> DISC AMT: $disc_amt <br/>";
-        //echo "<br/> AFTER DISC AMT: $charge - $disc_amt <br/>";
-
 
         // create the disc transaction
         //
@@ -263,37 +147,12 @@ else
 
     if ( $rt_remove_id != "" && $rt_remove_id > 0 )
     {
+        //echo "Removed discount";
         $query = "DELETE FROM reservation_transaction where id = $rt_remove_id";
 
         // die and show mysql error number and messages, if there is any error with query
         $result = mysql_query($query) or die ("ERROR: " . mysql_errno() . "-" . mysql_error() );
     }
-
-/* Moved to invoice_trans.php
-    echo "<table border=1><tr><th>Room Number</th><th>Amount</th><th>Remove</th></tr>";
-    $room_total_chg = 0;
-
-    // Show all transaction related to this room
-    $query = "SELECT rr.room_name, rt.amount
-                FROM reservation_transaction rt, reservation_room rr
-               WHERE rt.reservation_room_id = rr.id
-                 AND rt.reservation_entry_id = $reservation_id";
-
-    // die and show mysql error number and messages, if there is any error with query
-    $result = mysql_query($query) or die ("ERROR: " . mysql_errno() . "-" . mysql_error() );
-
-    if (mysql_num_rows($result)>0)
-    {
-        while($row = mysql_fetch_array($result))
-        {
-            echo "<tr>";
-            echo "<td>" . $row['room_name'] . "</td>";
-            echo "<td>" . "$" . number_format($row['amount'],2) . "</td>";
-            echo "</tr>";
-            $room_total_chg += $row['amount'];
-        }
-    }
-*/
 
     // Show all transaction with room id, misc charges of room rates
     $query = "SELECT mc.desc, rt.amount, rt.id AS rt_remove_id
